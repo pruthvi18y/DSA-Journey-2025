e4c0be52a6736ace45a9275be0cd90c19b40bb27
pruthvi18y <151669339+pruthvi18y@users.noreply.github.com>
2025-10-09 09:07:36 +0530

    Add files via upload

---
+++ Day26/Easy/Solution.java
@@ -0,0 +1,24 @@
+//Level : Easy
+//https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++) {
+            if (nums[i] == 1) {
+                count+=1;
+            }else {
+                if(max < count) {
+                    max = count;
+                }
+                count = 0;
+            }
+            
+        }
+        if (count > max) {
+            max = count;
+        }
+        return max;
+    }
+}
---
+++ Day26/Medium/Solution.java
@@ -0,0 +1,24 @@
+//Level : Medium
+//https://leetcode.com/problems/pascals-triangle/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int row = 0; row < numRows; row++) {
+            List<Integer> currentRow = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                // First and last elements are always 1
+                if (col == 0 || col == row) {
+                    currentRow.add(1);
+                } else {
+                    int val = triangle.get(row - 1).get(col - 1) + triangle.get(row - 1).get(col);
+                    currentRow.add(val);
+                }
+            }
+            triangle.add(currentRow);
+        }
+
+        return triangle;
+    }
+}
